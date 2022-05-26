89d3ae13c012ec5ecea747d4b8f0030c98b8d608
yq8519 <yq8518@hotmail.com>
2022-05-26 12:22:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/yq8519/luci-theme-argon-light-mod.git package/luci-theme-argon-light-mod
+git clone https://github.com/yq8519/luci-theme-argon-dark-mod.git package/luci-theme-argon-dark-mod
